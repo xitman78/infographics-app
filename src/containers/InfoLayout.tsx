b5ac76d5562d80e02e18d136aaf4512b173251b1
xitman78 <xitman78@gmail.com>
2019-04-10 12:28:52 +0300

    SideBar and TimeSerie search

--- src/containers/InfoLayout.tsx
+++ src/containers/InfoLayout.tsx
@@ -1,38 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Layout, Menu, Icon } from "antd";
 import BackgroundImage from "../components/BackgroundImage";
-import persistentData from "../services/PersistentData";
+import TimeSeriesSearch from "../components/TimeSeriesSearch";
 
-const { Header, Footer, Content } = Layout;
+const { Header, Footer, Content, Sider } = Layout;
 
 interface InfoLayoutProps {
   logoutAction: () => void;
 }
 
-const InfoLayout = ({ logoutAction }: InfoLayoutProps) => (
-  <Layout style={{ minHeight: "100vh" }}>
-    <Header>
-      <Menu
-        theme="dark"
-        mode="horizontal"
-        defaultSelectedKeys={["1"]}
-        style={{ lineHeight: "64px" }}
-      >
-        <Menu.Item key="1">Infographics</Menu.Item>
-        <Menu.Item key="2">
-          <Icon
-            type="arrow-right"
-            style={{ fontSize: "20px" }}
-            onClick={logoutAction}
-          />
-        </Menu.Item>
-      </Menu>
-    </Header>
-    <Content style={{ flex: 1, display: "flex", flexDirection: "column" }}>
-      <BackgroundImage />
-    </Content>
-    <Footer />
-  </Layout>
-);
+const InfoLayout: React.FC<InfoLayoutProps> = ({ logoutAction }) => {
+  const [isSideBarOpened, setIsSideBarOpened] = useState<boolean>(false);
+
+  return (
+    <Layout style={{ minHeight: "100vh" }}>
+      <Header>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={["1"]}
+          style={{ lineHeight: "64px" }}
+        >
+          <Menu.Item key="1">Infographics</Menu.Item>
+          <Menu.Item
+            key="2"
+            onClick={() => setIsSideBarOpened(!isSideBarOpened)}
+          >
+            <Icon type="search" style={{ fontSize: "20px" }} />
+          </Menu.Item>
+          <Menu.Item key="3" onClick={logoutAction}>
+            <Icon type="arrow-right" style={{ fontSize: "20px" }} />
+          </Menu.Item>
+        </Menu>
+      </Header>
+      <Layout hasSider={true}>
+        <Content style={{ flex: 1, display: "flex", flexDirection: "column" }}>
+          <BackgroundImage />
+        </Content>
+        <Sider
+          theme="light"
+          collapsible
+          collapsed={isSideBarOpened}
+          defaultCollapsed={true}
+          collapsedWidth={0}
+          width={300}
+          onCollapse={(collapsed: boolean) => setIsSideBarOpened(collapsed)}
+        >
+          <TimeSeriesSearch onTimeSerieSelected={() => null} />
+        </Sider>
+      </Layout>
+      <Footer />
+    </Layout>
+  );
+};
 
 export default InfoLayout;
